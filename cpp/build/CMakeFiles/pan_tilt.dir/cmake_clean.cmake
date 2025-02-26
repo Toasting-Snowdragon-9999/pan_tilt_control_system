a203edf9ac392243671cf032a3cf0c031ec06a30
Chris <phfae23@student.sdu.dk>
2025-02-26 09:12:02 +0100

    Working cmake and cpp

---
+++ cpp/build/CMakeFiles/pan_tilt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pan_tilt.dir/src/main.cpp.o"
+  "CMakeFiles/pan_tilt.dir/src/main.cpp.o.d"
+  "pan_tilt"
+  "pan_tilt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pan_tilt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
