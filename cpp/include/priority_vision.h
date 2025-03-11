5057b5221684eb62f9ae9def4e02545c2563568e
Chris <161033927+Toasting-Snowdragon-9999@users.noreply.github.com>
2025-03-11 22:32:55 +0100

    Add files via upload

---
+++ cpp/include/priority_vision.h
@@ -0,0 +1,43 @@
+#ifndef PRIORITY_VISION_H
+#define PRIORITY_VISION_H
+
+#include <optional>
+#include <chrono>
+#include <thread>
+
+#include "vision.h"
+
+#define MAX_PRIOS 1
+
+enum class Priority {
+    LOW = 0,
+    MEDIUM = 1,  
+    HIGH = 2
+};
+
+struct ColorThreshold {
+    std::vector<cv::Scalar> values;
+    Priority priority;
+    std::vector<std::vector<cv::Point>> contours;
+    double min_area;
+};
+
+
+class PrioVision : public Vision {
+    public:
+        PrioVision();
+        ~PrioVision();
+        void tracking () override;
+
+    protected: 
+        std::vector<ColorThreshold> _thresholds;
+        
+        void _priorities();
+        void _set_priority(bool run_calibration);
+        std::optional<std::reference_wrapper<ColorThreshold>> 
+        find_highest_priority_threshold(std::vector<ColorThreshold>& thresholds);
+        void _prio_calibration(); 
+
+};   
+
+#endif
