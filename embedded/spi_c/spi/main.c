0646f8c5f6499f23d10708ccdcc5a462b16bdcac
laurhol <lahol23@student.sdu.dk>
2025-03-12 16:58:50 +0100

    Created some of SPI initialization for Tiva

---
+++ embedded/spi_c/spi/main.c
@@ -0,0 +1,29 @@
+
+/***************** Header ********************/
+/*********************************************
+ * University of Southern Denmark
+ * Embedded Programming (EMP)
+ *
+ * MODULENAME: main.c
+ * PROJECT: Control of a pan-tilt system
+ * DESCRIPTION: ---
+ * Change Log:
+***********************************************
+* Date    Id    Change
+* YYMMDD: 2025/12/03
+* --------------------
+ *********************************************/
+
+/***************** Include files ******************/
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+#include "emp_type.h"
+/***************** Defines ********************/
+/***************** Constants ******************/
+/***************** Variables ******************/
+int main(void)
+{
+	return 0;
+}
+
+/***************** Enf of module **************/
