dcf63fe13cbde9b26ea9d4a85c4902697da86763
Lucas Rosenkvist <lucasrosenkvist7@gmail.com>
2025-05-07 15:36:38 +0200

    Updated PID controller with correct data types

--- embedded/RB4_v2/project_src/controller.c
+++ embedded/RB4_v2/project_src/controller.c
@@ -10,8 +10,8 @@
 
 
 void PID_Init(PIDController_t *pid,
-              double kp, double ki, double kd, double Ts, double N,
-              double output_min, double output_max)
+              FP32 kp, FP32 ki, FP32 kd, FP32 Ts, FP32 N,
+              FP32 output_min, FP32 output_max)
 {
     // Values given to the PID controller
     pid->kp = kp;
@@ -35,28 +35,28 @@ void PID_Init(PIDController_t *pid,
     // U_d(z) / e(z) = gamma * (z-1) / (alpha*z + beta)
 }
 
-double PID_Compute(PIDController_t *pid,
-                   double reference,
-                   double measured_value)
+INT16U PID_Compute(PIDController_t *pid,
+                    INT16U reference,
+                    INT16U measured_value)
 {
-    double error = reference - measured_value;
+    FP32 error = reference - measured_value;
 
         // Proportional
-        double P = pid->kp * error;
+        FP32 P = pid->kp * error;
 
         // Integral
-        //double I = pid->prev_integral + pid->ki * (pid->Ts / 2) * (error + pid->prev_error);
+        //FP32 I = pid->prev_integral + pid->ki * (pid->Ts / 2) * (error + pid->prev_error);
         //pid->prev_integral = I; // Save the integral term for next calculation
 
         // Derivative
-        double D = - (pid->alpha / pid->beta) * pid->prev_derivative + (pid->gamma / pid->beta) * (error - pid->prev_error);
+        FP32 D = - (pid->alpha / pid->beta) * pid->prev_derivative + (pid->gamma / pid->beta) * (error - pid->prev_error);
         pid->prev_derivative = D; // Save the derivative term for next calculation
 
         // Update previous error for next calculation
         pid->prev_error = error;
 
         // Compute PID output
-        double output = P + I + D;
+        INT16U output = (INT16U)(P /*+ I*/ + D);
 
         // Clamp output to min/max
         /*if (output > pid->output_max) {
@@ -73,9 +73,6 @@ double PID_Compute(PIDController_t *pid,
             pid->integral = pid->output_min;
         }*/
 
-        // Save current error for next derivative calculation
-        pid->prev_error = error;
-
         return output;
 
 }
@@ -85,10 +82,10 @@ void vPidControllerTask(void *pvParameters)
     //uart_putc('C');
 
     PIDController_t pid;
-    PID_Init(&pid,  1.0, 0.5, 0.1, 0.1, -10.0, 10.0);
-    double reference      = 100.0;
-    double measured_value =  0.0;
-    double output = 0.0;
+    PID_Init(&pid, 350.0, 0.1, 10.0, 0.0001, 1000.0, -12.0, 12.0);
+    FP32 reference      = 100.0;
+    FP32 measured_value =  0.0;
+    FP32 output = 0.0;
     uint8_t in;
 
     TickType_t xLastWakeTime = xTaskGetTickCount(); //initilize before start of task
@@ -111,7 +108,7 @@ void vPidControllerTask(void *pvParameters)
             measured_value = in;
         }
 
-        double output = PID_Compute(&pid, reference, measured_value);
+        FP32 output = PID_Compute(&pid, reference, measured_value);
 
         //controller output send through txQueue, and printed in txTask
         if(xQueueSend(xUartTxQueue, &output, pdMS_TO_TICKS(200)) == pdTRUE){  //macro for delay???
@@ -124,4 +121,3 @@ void vPidControllerTask(void *pvParameters)
 
 
 }
-
