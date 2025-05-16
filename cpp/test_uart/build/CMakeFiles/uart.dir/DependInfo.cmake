1dc73cabda7b794bb731b5145cb363776beaf80b
chris1501 <christopherfaerch@gmail.com>
2025-05-16 20:09:51 +0200

    working FPGA

---
+++ cpp/test_uart/build/CMakeFiles/uart.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rootlet/git/pan_tilt_control_system/cpp/test_uart/main_test_uart.cpp" "CMakeFiles/uart.dir/main_test_uart.cpp.o" "gcc" "CMakeFiles/uart.dir/main_test_uart.cpp.o.d"
+  "/home/rootlet/git/pan_tilt_control_system/cpp/test_uart/uart.cpp" "CMakeFiles/uart.dir/uart.cpp.o" "gcc" "CMakeFiles/uart.dir/uart.cpp.o.d"
+  "" "uart" "gcc" "CMakeFiles/uart.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
